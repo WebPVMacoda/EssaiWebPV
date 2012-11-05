84bc2e95647b226daad70ff1d232b274cbe0ccf9
BDPV <contact@bdpv.fr>
2012-11-05 20:58:43 +0100

    Changement radical sur index.php pour publication via FTP

--- A_effacer_projet/index.php
+++ A_effacer_projet/index.php
@@ -1,6 +1,26 @@
-<?php
-echo "hello";
-echo "This is a new line";
-echo "Another line";
-echo "Depuis le Mac";
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+<title>WebPV - Web services for photovoltaic monitoring</title>
+</head>
+<body>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<center><h1><i><font size = 50>Web</font></i><b><font size = 50 color="blue">PV</font></b></h1>
+<p><font size = 5>Web services for the automatic detection of failures and performance problems of photovoltaic systems</font></p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<div id="Login">
+<p><b>CLIENT ACCESS</b></p>
+<FORM METHOD = POST  ACTION = 'index.php'>
+				<p>User:<br>
+				<INPUT NAME = 'user'  TYPE = TEXT MAXLENGTH = 20  SIZE = 20><br>
+				Password:<br>
+				<INPUT NAME = 'pass'  TYPE = PASSWORD  MAXLENGTH = 20  SIZE = 20><br>
+				<INPUT TYPE = SUBMIT  VALUE = 'Connect'></p>
+				</FORM>
+</div>
+</center>
+</body>
+</html>
